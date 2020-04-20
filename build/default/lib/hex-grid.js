967d9dbb05012435607ec411afa688f927b8447d
Peter Pham <ptr.pham@gmail.com>
2020-04-19 17:29:11 -0700

    Initial pages release.

---
+++ build/default/lib/hex-grid.js
@@ -0,0 +1,405 @@
+import { LitElement, html, css, svg } from "../node_modules/lit-element/lit-element.js";
+import { styleMap } from "../node_modules/lit-html/directives/style-map.js";
+let HEX_ANGLES = new Array(6).fill(0).map((x, i) => 2 * Math.PI * (i + 0.5) / 6);
+let HEX_ANGLES_COS = HEX_ANGLES.map(x => Math.cos(x));
+let HEX_ANGLES_SIN = HEX_ANGLES.map(x => Math.sin(x));
+let HEX_ADJACENT = [[1, 0], [-1, 0], [0, 1], [0, -1], [1, -1], [-1, 1]];
+
+function renderHexPath(id, shape, style = {}) {
+  let {
+    cx,
+    cy,
+    r
+  } = shape;
+  let {
+    fill = 'whitesmoke',
+    opacity = 1
+  } = style;
+  let corners = HEX_ANGLES.map(x => [r * Math.cos(x) + cx, r * Math.sin(x) + cy]);
+  let coords = corners.map(x => x.join(','));
+  let d = `M${coords[0]} ${coords.join('L ')} Z`;
+  return svg`<path class="hex" data-id="${id}" d="${d}"
+      style="${styleMap({
+    opacity,
+    fill
+  })}"></path>`;
+}
+
+function renderEdgePath(id, shape, style = {}) {
+  let {
+    x0,
+    y0,
+    x1,
+    y1
+  } = shape;
+  let {
+    stroke = 'lightgray',
+    strokeWidth = 1
+  } = style;
+  return svg`
+    <line class="edge" data-id="${id}" x1="${x0}" y1="${y0}" x2="${x1}" y2="${y1}" stroke="${stroke}"
+      stroke-width="${strokeWidth}"></line>
+  `;
+}
+
+function renderCorner(id, shape, style = {}) {
+  let {
+    cx,
+    cy,
+    r
+  } = shape;
+  let {
+    fill = 'lightgray'
+  } = style;
+  return svg`
+    <circle class="corner" data-id="${id}" cx="${cx}" cy="${cy}" r="${r}"
+       style="${styleMap({
+    fill
+  })}"></line>
+  `;
+}
+
+function commaCount(x) {
+  let result = 0;
+
+  for (let i = 0; i < x.length; i++) {
+    if (x[i] == ',') result++;
+  }
+
+  return result;
+}
+
+function isValidCoord(ns, i, j) {
+  return !(i + j < ns - 1 || i + j > 2 * ns + Math.ceil(ns / 2) - 1);
+}
+
+export function validHexCoords(ns) {
+  let result = [];
+
+  for (let i = 0; i < 2 * ns - 1; i++) {
+    for (let j = 0; j < 2 * ns - 1; j++) {
+      if (!isValidCoord(ns, i, j)) continue;
+      result.push([i, j]);
+    }
+  }
+
+  return result;
+}
+
+function coordToId(coord) {
+  return JSON.stringify(coord);
+}
+
+function validHalfedgeCoords(ns) {
+  const allHalfedges = validHexCoords(ns).flatMap(x => HEX_ANGLES.map((y, i) => [...x, i]));
+  const canonicalIds = allHalfedges.map(x => coordToId(canonicalHalfedge(x)));
+  return Array.from(new Set(canonicalIds)).map(x => JSON.parse(x));
+}
+
+function canonicalHalfedge(coord) {
+  if (coord[2] < 3) return coord.length == 3 ? coord : coord.slice(0, 3);
+  return halfedgeMirror(coord);
+}
+
+export function validCoords(ns) {
+  return [...validHexCoords(ns), ...validHalfedgeCoords(ns)];
+}
+
+function halfedgeMirror(coord) {
+  switch (coord[2]) {
+    case 0:
+      return [coord[0], coord[1] + 1, 3];
+
+    case 1:
+      return [coord[0] - 1, coord[1] + 1, 4];
+
+    case 2:
+      return [coord[0] - 1, coord[1], 5];
+
+    case 3:
+      return [coord[0], coord[1] - 1, 0];
+
+    case 4:
+      return [coord[0] + 1, coord[1] - 1, 1];
+
+    case 5:
+      return [coord[0] + 1, coord[1], 2];
+  }
+}
+
+export function hexCenterXFunction(ns, r, margin = 0) {
+  let dx = Math.cos(HEX_ANGLES[0]);
+  let scale = r + margin;
+  return (i, j) => scale * dx * (2 * i + j - ns + 2.5);
+}
+export function hexCenterYFunction(ns, r, margin = 0) {
+  let dy = Math.sin(HEX_ANGLES[0]) + 1;
+  let scale = r + margin;
+  return (i, j) => scale * (dy * j + 2) + 2 * margin;
+}
+export class HexGrid extends LitElement {
+  static get properties() {
+    return {
+      ns: {
+        type: Number
+      },
+      margin: {
+        type: Number
+      },
+      r: {
+        type: Number
+      },
+      elementStyles: {
+        type: Object
+      },
+      offsetX: {
+        type: Number
+      },
+      offsetY: {
+        type: Number
+      }
+    };
+  }
+
+  static get styles() {
+    return css`
+      :host {
+        display: block;
+      }
+
+      svg {
+        width: 100%;
+        height: 100%;
+        filter: drop-shadow(2px 4px 6px rgba(0,0,0,0.5));
+      }
+
+      .hex {
+        transition: fill 0.2s;
+        fill: whitesmoke;
+      }
+
+      .corner, .edge {
+        transition: stroke 0.2s;
+        fill: lightgray;
+      }
+    `;
+  }
+
+  isValidCoord(i, j) {
+    return isValidCoord(this.ns, i, j);
+  }
+
+  dispatchGenericMouseEvent(e, name) {
+    let tileType = null;
+    if (e.target.matches('.hex')) tileType = 'hex';
+    if (e.target.matches('.edge')) tileType = 'edge';
+
+    if (tileType) {
+      let id = e.target.dataset.id;
+      let detail = {
+        id,
+        coord: JSON.parse(id),
+        tileType
+      };
+      this.dispatchEvent(new CustomEvent(`tile-${name}`, {
+        detail
+      }));
+    }
+  }
+
+  onClick(e) {
+    this.dispatchGenericMouseEvent(e, 'click');
+  }
+
+  onMouseOver(e) {
+    this.dispatchGenericMouseEvent(e, 'mouseover');
+  }
+
+  onMouseOut(e) {
+    this.dispatchGenericMouseEvent(e, 'mouseout');
+  }
+
+  get validHexCoords() {
+    return validHexCoords(this.ns);
+  }
+
+  canonizeCorner(coord) {
+    if (coord[2] == 2 || coord[2] == 5) return [...coord, 0];
+
+    if (coord[2] == 0) {
+      return [...this.halfedgeMirror(this.halfedgePrev(coord)), 0];
+    } else if (coord[2] == 1) {
+      return [...this.halfedgeNext(this.halfedgeMirror(coord)), 0];
+    } else if (coord[2] == 3) {
+      return [...this.halfedgeMirror(this.halfedgePrev(coord)), 0];
+    } else if (coord[2] == 4) {
+      return [...this.halfedgeNext(this.halfedgeMirror(coord)), 0];
+    }
+  }
+
+  get validCornerIds() {
+    return Array.from(new Set(this.validHexCoords.flatMap(x => HEX_ANGLES.map((y, i) => this.coordToId(this.canonizeCorner([...x, i]))))));
+  }
+
+  get validHalfedgeCoords() {
+    const allHalfedges = this.validHexCoords.flatMap(x => HEX_ANGLES.map((y, i) => [...x, i]));
+    const canonicalIds = allHalfedges.map(x => this.coordToId(this.canonicalHalfedge(x)));
+    return Array.from(new Set(canonicalIds)).map(x => JSON.parse(x));
+  }
+
+  get validCoords() {
+    return [...this.validHexCoords, ...this.validHalfedgeCoords];
+  }
+
+  halfedgeMirror(coord) {
+    return halfedgeMirror(coord);
+  }
+
+  halfedgeNext(coord) {
+    return [coord[0], coord[1], (coord[2] + 1) % 6];
+  }
+
+  halfedgePrev(coord) {
+    return [coord[0], coord[1], (coord[2] + 5) % 6];
+  }
+
+  hexAdjacentHexes(coord) {
+    return HEX_ADJACENT.map(([dx, dy]) => [coord[0] + dx, coord[1] + dy]);
+  }
+
+  hexAdjacentEdges(coord) {
+    return HEX_ANGLES.map((x, i) => this.canonicalHalfedge([...coord, i]));
+  }
+
+  hexAdjacentHexInDirection(coord, i) {
+    return this.halfedgeMirror([...coord, i]).slice(0, 2);
+  }
+
+  halfedgesAdjacentEdges(coord) {
+    let mirror = this.halfedgeMirror(coord);
+    return [this.halfedgeNext(coord), this.halfedgePrev(mirror), this.halfedgePrev(coord), this.halfedgeNext(mirror)].map(x => this.canonicalHalfedge(x));
+  }
+
+  edgeAdjacentHexes(coord) {
+    let mirror = this.halfedgeMirror(coord);
+    return [coord.slice(0, 2), mirror.slice(0, 2)];
+  }
+
+  cornerAdjacentEdges(coord) {
+    let prev = this.halfedgePrev(coord);
+    let nextMirror = this.halfedgeNext(this.halfedgeMirror(coord));
+    return [coord, prev, nextMirror].map(x => this.canonicalHalfedge(x));
+  }
+
+  canonicalHalfedge(coord) {
+    return canonicalHalfedge(coord);
+  }
+
+  coordToId(coord) {
+    return JSON.stringify(coord);
+  }
+
+  idToCoord(id) {
+    return JSON.parse(id);
+  }
+
+  isEdgeId(id) {
+    return commaCount(id) == 2;
+  }
+
+  isHexId(id) {
+    return commaCount(id) == 1;
+  }
+
+  get hexCenterXFunction() {
+    let {
+      ns,
+      r,
+      margin = 0,
+      offsetX = 0
+    } = this;
+    let inner = hexCenterXFunction(ns, r, margin);
+    return (i, j) => inner(i, j) + offsetX;
+  }
+
+  get hexCenterYFunction() {
+    let {
+      ns,
+      r,
+      margin = 0,
+      offsetY = 0
+    } = this;
+    let inner = hexCenterYFunction(ns, r, margin);
+    return (i, j) => inner(i, j) + offsetY;
+  }
+
+  render() {
+    let hexes = [];
+    let {
+      ns,
+      r,
+      margin = 0,
+      elementStyles
+    } = this;
+    let scale = r + margin;
+    let computeCX = this.hexCenterXFunction;
+    let computeCY = this.hexCenterYFunction;
+
+    for (let [i, j] of this.validHexCoords) {
+      let cx = computeCX(i, j);
+      let cy = computeCY(i, j);
+      let shape = {
+        cx,
+        cy,
+        r
+      };
+      let id = this.coordToId([i, j]);
+      let style = this.elementStyles.get(id) || {};
+      hexes.push(renderHexPath(id, shape, style));
+    }
+
+    let edges = [];
+
+    for (let [i, j, k] of this.validHalfedgeCoords) {
+      let cx = computeCX(i, j);
+      let cy = computeCY(i, j);
+      let x0 = cx + scale * HEX_ANGLES_COS[k];
+      let y0 = cy + scale * HEX_ANGLES_SIN[k];
+      let x1 = cx + scale * HEX_ANGLES_COS[(k + 1) % 6];
+      let y1 = cy + scale * HEX_ANGLES_SIN[(k + 1) % 6];
+      let shape = {
+        x0,
+        y0,
+        x1,
+        y1
+      };
+      let id = this.coordToId([i, j, k]);
+      let style = this.elementStyles.get(id) || {};
+      style.strokeWidth = 2 * this.margin;
+      edges.push(renderEdgePath(id, shape, style));
+    }
+
+    let corners = [];
+
+    for (let id of this.validCornerIds) {
+      let [i, j, k] = this.idToCoord(id);
+      let cx = computeCX(i, j);
+      let cy = computeCY(i, j);
+      corners.push(renderCorner(id, {
+        cx: cx + scale * HEX_ANGLES_COS[k],
+        cy: cy + scale * HEX_ANGLES_SIN[k],
+        r: this.margin
+      }, this.elementStyles.get(id) || {}));
+    }
+
+    return html`
+      <svg viewBox="0 0 100 100"
+        @click="${this.onClick}" @mouseover="${this.onMouseOver}" @mouseout="${this.onMouseOut}">
+          ${edges}
+          ${corners}
+          ${hexes}
+      </svg>`;
+  }
+
+}
+customElements.define('hex-grid', HexGrid);
---
+++ build/default/lib/main.js
@@ -0,0 +1,390 @@
+import { PLAYER_COLOR_MAP } from './hexa-slime-game.js';
+import { LitElement, html, css, svg } from "../node_modules/lit-element/lit-element.js";
+import { styleMap } from "../node_modules/lit-html/directives/style-map.js";
+
+class ModalFrame extends LitElement {
+  static get properties() {
+    return {
+      show: {
+        type: Boolean
+      }
+    };
+  }
+
+  static get styles() {
+    return css`
+      #shade {
+        position: fixed;
+        width: 100vw;
+        height: 100vh;
+        background: rgba(0,0,0,0.8);
+        transition: background 0.2s;
+        z-index: 2000;
+      }
+
+      #shade.hidden {
+        pointer-events: none;
+        background: transparent;
+      }
+
+      #wrapper {
+        height: 80%;
+        width: 70%;
+        background: white;
+        border-radius: 20pt;
+        box-shadow: 0 0 10pt 0 black;
+        position: fixed;
+        top: 10%;
+        left: 15%;
+        padding: 20pt;
+        box-sizing: border-box;
+
+        display: flex;
+        flex-direction: column;
+        justify-content: space-between;
+        transition: opacity 0.2s;
+        opacity: 1;
+      }
+
+      .hidden #wrapper {
+        opacity: 0;
+      }
+    `;
+  }
+
+  render() {
+    let showClass = this.show ? '' : 'hidden';
+    return html`
+      <div id="shade" class="${showClass}">
+        <div id="wrapper"><slot></slot>
+        </div>
+      </div>`;
+  }
+
+}
+
+customElements.define('modal-frame', ModalFrame);
+export class HexaSlimeGameManager extends LitElement {
+  static get properties() {
+    return {
+      showModal: {
+        type: Boolean
+      },
+      winners: {
+        type: Array
+      },
+      peerId: {
+        type: String
+      },
+      serverId: {
+        type: String
+      },
+      connectedServerId: {
+        type: String
+      },
+      connectedPeerIds: {
+        type: Array
+      },
+      currentPlayer: {
+        type: String
+      },
+      players: {
+        type: Array
+      }
+    };
+  }
+
+  static get styles() {
+    return css`
+      :host {
+        font-weight: lighter;
+      }
+
+      button {
+        font-size: 20pt;
+        padding: 0 15pt;
+        border: 1pt solid gray;
+        box-shadow: none;
+        transition: box-shadow 0.2s;
+        cursor: pointer;
+        border-radius: 50pt;
+        font-weight: lighter;
+        outline: none;
+      }
+
+      button + button {
+        margin-top: 10pt;
+      }
+
+      button:hover {
+        color: white;
+        background: dimgray;
+      }
+
+      hex-grid {
+        flex: 1;
+      }
+
+      #intro {
+        display: flex;
+        flex-direction: column;
+        height: 100%;
+      }
+
+      #instructions {
+        overflow-y: auto;
+        box-shadow: 0 0 8pt 0 rgba(0,0,0,0.2) inset;
+        padding: 0 10pt;
+        padding-bottom: 10pt;
+        margin-bottom: 20pt;
+      }
+
+      input {
+        width: 100%;
+        border: 1pt solid gray;
+        font-size: 20pt;
+        border-radius: 100pt;
+        padding: 5pt 20pt;
+        outline: none;
+        margin-bottom: 20pt;
+      }
+
+      #player-list {
+        margin-bottom: 20pt;
+      }
+
+      .message {
+        margin-top: 10pt;
+      }
+    `;
+  }
+
+  constructor() {
+    super();
+    this.showModal = 'intro';
+    this.players = ['red', 'blue'];
+  }
+
+  get game() {
+    return this.shadowRoot.querySelector('hexa-slime-game');
+  }
+
+  startLocalGame() {
+    let game = this.game;
+    game.players = this.players;
+    game.restart();
+    this.showModal = false;
+    this.gameMode = 'local';
+  }
+
+  startNetworkGame() {
+    this.startAsClient(this.players[0]);
+    this.currentPlayer = this.players[0];
+    this.dispatchEvent(new CustomEvent('server-start'));
+  }
+
+  startAsClient(player) {
+    let game = this.game;
+    game.players = this.players;
+    this.gameMode = 'network';
+    game.restart();
+    this.showModal = false;
+    this.clientPlayer = player;
+  }
+
+  renderModal() {
+    if (!this.showModal) return null;
+
+    switch (this.showModal) {
+      case 'intro':
+        return this.renderIntro();
+
+      case 'game-over':
+        return this.renderGameOver();
+
+      case 'network-server':
+        return this.renderServerGame();
+
+      case 'network-client':
+        return this.renderClientGame();
+    }
+
+    return null;
+  }
+
+  returnToIntro() {
+    this.showModal = 'intro';
+  }
+
+  renderGameOver() {
+    let winners = this.winners || [];
+    let heading = winners.length ? 'WINNER' : 'NO WINNERS';
+    let elementStyles = new Map();
+    let player = winners[0] || 'dead';
+    elementStyles.set('[0,0]', {
+      fill: PLAYER_COLOR_MAP[player].hexColor,
+      stroke: PLAYER_COLOR_MAP[player].edgeColor
+    });
+    return html`
+      <h1 style="align-self: center">${heading}</h1>
+      <hex-grid r=30 ns=1 margin=5 .elementStyles="${elementStyles}"
+        .offsetX="${5}" .offsetY="${-30}"></hex-grid>
+      <button @click="${this.returnToIntro}">RETURN TO INTRO</button>
+    `;
+  }
+
+  configureNetworkGame() {
+    this.showModal = 'network-server';
+  }
+
+  handleRemoteTileClick(detail) {
+    this.game.processTileClick(detail);
+  }
+
+  renderIntro() {
+    return html`
+      <div id="intro">
+        <h1>HEXASLIME</h1>
+
+        <div id="instructions">
+        <h3>INTRO</h3>
+        Dr. Slimentist was working late in his lab one night and created
+        sentient slime blobs in this giant novelty, hexagonally tiled petri
+        dish gifted to him by his Aunt Bertrude for Christmas. He watches on in
+        fascination as the slimes struggle to keep themselves alive.
+
+        <h3>RULES</h3>
+        Each turn, you may take one of two actions:
+        <ul>
+          <li>Click on a hex tile to claim it.</li>
+          <li>Click on a edges to claim them. Edges must be adjacent to hexes
+          or edges that you already own. If you choose to do this, you must
+          claim the number of edges associated with your color on the left if
+          there are available unclaimed edges.</li>
+          <li>You gain one usable edge per turn.</li>
+        </ul>
+
+        Connected hexes and edges make blobs:
+        <ul>
+          <li> Two adjacent hexes of the same color are connected if the edge
+            between them is claimed by the same player or is unclaimed.</li>
+          <li> A hex is connected to an edge if the edge is one of the hex's
+            edges.</li>
+          <li> Edges are connected if they share a corner.</li>
+        </ul>
+
+        Keep your blobs alive:
+        <ul>
+          <li>Each blob has health that decreases every round.</li>
+          <li>When a blob's health drops below zero, it dies. </li>
+          <li>A blob's health is shown by a number on the newest hex in the
+            blob.</li>
+          <li>When a blob gets an additional hex, it's health increases to the
+            number of hexes that compose it.</li>
+          <li>Blobs may not have more health than the number of hexes that
+          compose it. This means that splitting blobs can make them lose
+          health.</li>
+          <li> Two blobs connected by an edge will take on the maximum health
+            between them.</li>
+        </ul>
+
+        Win the game:
+        <ul>
+          <li>The player with the last living blob on the field wins.</li>
+          <li>If there are no living blobs, there is no winner.</li>
+        </ul>
+
+        </div>
+        <button @click="${this.startLocalGame}">START LOCAL GAME</button>
+        <button @click="${this.configureGameAsServer}">NETWORK GAME AS SERVER</button>
+        <button @click="${this.configureGameAsClient}">NETWORK GAME AS CLIENT</button>
+      </div>
+    `;
+  }
+
+  configureGameAsServer() {
+    this.showModal = 'network-server';
+    this.dispatchEvent(new CustomEvent('server-setup'));
+    this.isServer = true;
+  }
+
+  configureGameAsClient() {
+    this.showModal = 'network-client';
+    this.isServer = false;
+  }
+
+  updateServerId(e) {
+    this.serverId = e.target.value;
+  }
+
+  connectToServer() {
+    if (this.serverId && this.serverId != this.peerId) {
+      this.dispatchEvent(new CustomEvent('server-connect', {
+        detail: {
+          serverId: this.serverId
+        }
+      }));
+    }
+  }
+
+  renderServerGame() {
+    let peerId = this.peerId || '[Loading...]';
+    let emptyMessage = !this.connectedPeerIds || !this.connectedPeerIds.length ? '[No Players Connected]' : '';
+    return html`
+      <h1>NETWORK GAME AS SERVER</h1>
+      <div>
+        <p>Your server id is <b>${peerId}</b>. Send this
+        value to other players to connect to your machine</p>
+        <h3>CONNECTED PLAYERS</h3>
+        <div id="player-list">
+          ${emptyMessage} 
+          ${(this.connectedPeerIds || []).map(id => html`<b>${id}</b>`)}
+        </div>
+        <button @click="${this.startNetworkGame}">START NETWORK GAME</button>
+      </div>
+      <button @click="${this.returnToIntro}">RETURN TO INTRO</button>
+    `;
+  }
+
+  renderCurrentServerConnection() {
+    if (!this.connectedServerId) return null;
+    return html`<div class="message">Connected to server <b>${this.connectedServerId}</b></div>`;
+  }
+
+  renderClientGame() {
+    return html`
+      <h1>NETWORK GAME AS CLIENT</h1>
+      <div>
+        <p>Your id is <b>${this.peerId}</b>. Ask for the serving player's server id
+        and enter it below.</p>
+        <input value="${this.serverId || ''}" @input="${this.updateServerId}"></input>
+        <button @click="${this.connectToServer}">CONNECT TO SERVER</button>
+        ${this.renderCurrentServerConnection()}
+      </div>
+      <button @click="${this.returnToIntro}">RETURN TO INTRO</button>
+    `;
+  }
+
+  onGameOver(e) {
+    this.winners = e.detail.winners;
+    this.showModal = 'game-over';
+  }
+
+  onNextTurn(e) {
+    this.currentPlayer = e.detail.currentPlayer;
+  }
+
+  render() {
+    let clientIsPlayer = this.gameMode == 'local' || this.clientPlayer && this.currentPlayer == this.clientPlayer;
+    return html`
+      <modal-frame .show=${this.showModal}>
+        ${this.renderModal()}
+      </modal-frame>
+      <hexa-slime-game currentPlayerIndex=0 .players="${this.players}"
+        @game-over=${this.onGameOver} @next-turn="${this.onNextTurn}"
+        .clientIsPlayer="${clientIsPlayer}"></hexa-slime-game>
+    `;
+  }
+
+}
+customElements.define('hexa-slime-game-manager', HexaSlimeGameManager);
