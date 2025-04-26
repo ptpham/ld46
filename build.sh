bb12b289a82c3e3a55a221c2642fac10f9fa101f
ptpham <ptr.pham@gmail.com>
2025-04-26 15:26:52 -0700

    Using esbuild instead

--- build.sh
+++
@@ -1,5 +0,0 @@
-
-rm dist
-mkdir dist/lib
-esbuild ./lib/main.js > dist/lib/main.js --bundle
-cp main.html dist/main.html
---
+++ build.ts
@@ -0,0 +1,10 @@
+import * as esbuild from "https://deno.land/x/esbuild@v0.25.0/mod.js";
+
+await esbuild.build({
+  entryPoints: ["./lib/main.js"],
+  bundle: true,
+  platform: "browser",
+  format: "esm",
+  outfile: "dist/lib/main.js",
+  sourcemap: true,
+});
